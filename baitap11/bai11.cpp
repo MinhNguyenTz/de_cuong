c2185cf28167e7b3108823bf3a40d5195f6e3d12
SkadiNguyen <joseph.anben@gmail.com>
2022-10-13 13:46:55 +0700

    13/10/2022

--- baitap11/bai11.cpp
+++ baitap11/bai11.cpp
@@ -14,11 +14,25 @@ namespace {
 }
 
 const int maxN = 1e5 + 7;
+unordered_map<ll,ll> memo;
+
+ll fib(ll x){
+	if (x <= 0) return 0;
+	if (x == 1) return 1;
+	if (memo.count(x)) return memo[x];
+
+	ll result = fib(x - 1) + fib(x - 2);
+	memo[x] = result;
+	return result;
+}
 
 int main(){
 	ios::sync_with_stdio(NULL); cin.tie(NULL); cout.tie(NULL);
-    freopen ("bai.inp", "r", stdin);
-    freopen ("bai.out", "w", stdout);
+    freopen ("bai11.inp", "r", stdin);
+    freopen ("bai11.out", "w", stdout);
 			
+	ll n; cin >> n;
+	cout << fib(n);
+	// Bị tràn
 	return 0;
 }
--- baitap12/bai12.cpp
+++ baitap12/bai12.cpp
@@ -17,8 +17,24 @@ const int maxN = 1e5 + 7;
 
 int main(){
 	ios::sync_with_stdio(NULL); cin.tie(NULL); cout.tie(NULL);
-    freopen ("bai.inp", "r", stdin);
-    freopen ("bai.out", "w", stdout);
+    freopen ("bai12.inp", "r", stdin);
+    freopen ("bai12.out", "w", stdout);
 			
+	unordered_map <int,ll> track;
+	int n; cin >> n;
+	while (n--){
+		int a; cin >> a;
+		track[a]++;
+	}
+
+	ll m, cur_m = LONG_LONG_MIN;
+	for (auto x: track){
+		if (x.se > cur_m){
+			cur_m = x.se;
+			m = x.ft;
+		}
+	}
+
+	cout << m;
 	return 0;
 }
